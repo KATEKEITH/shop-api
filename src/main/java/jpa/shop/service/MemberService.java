af5bf3284a3ea4ad78636a4af79fe4901445e67f
심은지 <simbomi06@gmail.com>
2022-11-10 11:22:49 +0900

    feat member repository, service

---
+++ src/main/java/jpa/shop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpa.shop.service;
+
+import jpa.shop.domain.Member;
+import jpa.shop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMember = memberRepository.findByName(member.getName());
+
+        if(!findMember.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+      */
+    @Transactional(readOnly = true)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+}
